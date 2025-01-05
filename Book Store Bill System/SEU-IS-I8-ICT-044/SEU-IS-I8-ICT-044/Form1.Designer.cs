3f9be2534b1b6581093e573dcd03a71dbe15228f
nayanajithedirisinghe <nayanajithedirisinha@gmail.com>
2025-01-05 12:09:16 +0530

    first commit

---
+++ Book Store Bill System/SEU-IS-I8-ICT-044/SEU-IS-I8-ICT-044/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SEU_IS_I8_ICT_044
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxBook = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxMembership = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Quantity = new System.Windows.Forms.Label();
+            this.Price1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblBreakdown = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bookstore Billing System";
+            // 
+            // comboBoxBook
+            // 
+            this.comboBoxBook.FormattingEnabled = true;
+            this.comboBoxBook.Items.AddRange(new object[] {
+            "The Lord of the Rings",
+            "Harry Potter and the Sorcerer\'s Stone",
+            "Pride and Prejudice",
+            "To Kill a Mockingbird",
+            "The Great Gatsby",
+            "The Catcher in the Rye",
+            "1984",
+            "The Hitchhiker\'s Guide to the Galaxy",
+            "The Alchemist",
+            "The Silent Patient"});
+            this.comboBoxBook.Location = new System.Drawing.Point(93, 56);
+            this.comboBoxBook.Name = "comboBoxBook";
+            this.comboBoxBook.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBook.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Book";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Quantity";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(340, 55);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 4;
+            this.txtQuantity.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(493, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Membership Status";
+            // 
+            // comboBoxMembership
+            // 
+            this.comboBoxMembership.FormattingEnabled = true;
+            this.comboBoxMembership.Items.AddRange(new object[] {
+            "Non-Member",
+            "Club Member"});
+            this.comboBoxMembership.Location = new System.Drawing.Point(596, 55);
+            this.comboBoxMembership.Name = "comboBoxMembership";
+            this.comboBoxMembership.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMembership.TabIndex = 6;
+            this.comboBoxMembership.SelectedIndexChanged += new System.EventHandler(this.comboBoxMembership_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(191, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Available Quantity:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(402, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = " Price (USD):";
+            // 
+            // Quantity
+            // 
+            this.Quantity.AutoSize = true;
+            this.Quantity.Location = new System.Drawing.Point(311, 93);
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Size = new System.Drawing.Size(19, 13);
+            this.Quantity.TabIndex = 9;
+            this.Quantity.Text = "00";
+            // 
+            // Price1
+            // 
+            this.Price1.AutoSize = true;
+            this.Price1.Location = new System.Drawing.Point(489, 93);
+            this.Price1.Name = "Price1";
+            this.Price1.Size = new System.Drawing.Size(19, 13);
+            this.Price1.TabIndex = 10;
+            this.Price1.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Total";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblBreakdown
+            // 
+            this.lblBreakdown.Location = new System.Drawing.Point(263, 185);
+            this.lblBreakdown.Name = "lblBreakdown";
+            this.lblBreakdown.Size = new System.Drawing.Size(284, 210);
+            this.lblBreakdown.TabIndex = 25;
+            this.lblBreakdown.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBreakdown);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Price1);
+            this.Controls.Add(this.Quantity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxMembership);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxBook);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxBook;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxMembership;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Quantity;
+        private System.Windows.Forms.Label Price1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox lblBreakdown;
+    }
+}
+
---
+++ Book Store Bill System/SEU-IS-I8-ICT-044/SEU-IS-I8-ICT-044/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEU_IS_I8_ICT_044
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+        public class Book
+        {
+            public string Name { get; set; }
+            public int AvailableQuantity { get; set; }
+            public decimal Price { get; set; }
+
+            public Book(string name, int availableQuantity, decimal price)
+            {
+                Name = name;
+                AvailableQuantity = availableQuantity;
+                Price = price;
+            }
+        }
+        List<Book> books = new List<Book>
+        {
+            new Book("The Lord of the Rings", 25, 19.99m),
+            new Book("Harry Potter and the Sorcerer's Stone", 30, 12.99m),
+            new Book("To Kill a Mockingbird", 15, 8.99m),
+            new Book("The Great Gatsby", 20, 11.99m),
+            new Book("The Catcher in the Rye", 18, 9.99m),
+            new Book("1984", 22, 10.99m),
+            new Book("The Hitchhiker's Guide to the Galaxy", 10, 7.99m),
+            new Book("The Alchemist", 12, 14.99m),
+            new Book("The Silent Patient", 15, 12.99m)
+         };
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+        }
+       
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Quantity.Text = {books.Capacity};
+
+
+            // Validate inputs
+            if (comboBoxBook.SelectedIndex == -1 || string.IsNullOrEmpty(txtQuantity.Text) || comboBoxMembership.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            string selectedBook = comboBoxBook.SelectedItem.ToString();
+            int quantity = int.Parse(txtQuantity.Text);
+            string membership = comboBoxMembership.SelectedItem.ToString();
+
+            // Find the selected book
+            Book book = books.FirstOrDefault(b => b.Name == selectedBook);
+
+            if (book == null)
+            {
+                MessageBox.Show("Book not found.");
+                return;
+            }
+
+            if (quantity > book.AvailableQuantity)
+            {
+                MessageBox.Show("Quantity exceeds available stock.");
+                return;
+            }
+
+            // Calculate total price
+            decimal totalPrice = book.Price * quantity;
+
+            // Apply discounts
+            decimal discount = 0;
+
+            // Bulk Purchase Discount
+            if (totalPrice >= 50 && membership != "Club Member")
+            {
+                discount = totalPrice * 0.10m;
+            }
+            else
+            {
+                // Membership Discount
+                if (membership == "Club Member")
+                {
+                    discount = totalPrice * 0.10m;
+                }
+
+                // Other Discounts
+                if (selectedBook == "Harry Potter and the Sorcerer's Stone")
+                {
+                    discount += totalPrice * 0.12m;
+                }
+                else if (selectedBook == "The Hitchhiker's Guide to the Galaxy" || selectedBook == "The Alchemist")
+                {
+                    discount += totalPrice * 0.15m;
+                }
+
+                // Ensure no more than 2 discounts are applied
+                discount = Math.Min(discount, totalPrice * 0.27m); // Max 27% discount
+            }
+
+            decimal finalPrice = totalPrice - discount;
+
+            Price1.Text = $"{book.Price}";
+            Quantity.Text = $"{book.AvailableQuantity}";
+
+            // Display breakdown
+            lblBreakdown.Text = $"Book Name: {book.Name}\n" +
+                                $"Quantity: {quantity}\n" +
+                                $"Price per item: {book.Price:C}\n" +
+                                $"Total Price (Before Discount): {totalPrice:C}\n" +
+                                $"Discount Applied: {discount:C}\n" +
+                                $"Total Price (After Discount): {finalPrice:C}";
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //foreach (var book in books)
+            //{
+            //    comboBoxBook.Items.Add(book.Name);
+            //}
+            //comboBoxMembership.Items.Add("Club Member");
+            //comboBoxMembership.Items.Add("Non-Member");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBoxBook.SelectedIndex = -1;
+            txtQuantity.Clear();
+            comboBoxMembership.SelectedIndex = -1;
+            lblBreakdown.Text = "";
+        }
+
+        private void comboBoxMembership_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
